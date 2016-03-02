8375660ae4d1d41ad480aab82743e5464bbedc3e
Yliya <skripka696@ukr.net>
2016-03-02 15:14:50 +0200

    jquery templates

--- note.js
+++ note.js
@@ -13,7 +13,7 @@ var notes = [
     }
 
 ];
-
+var id = 0;
 var list_of_notes = $('#list_of_notes');
 
 var form = $('#form_note');
@@ -32,8 +32,9 @@ var set_notes = function (notes_l) {
 };
 
 var add_notes = function (notes_l) {
-    var new_note = $('#add_note').val();
-    notes_l.push({text: new_note});
+    var new_text = $('#add_text').val();
+    var new_title = $('#add_title').val();
+    notes_l.push({id: id+1, title: new_title, text: new_text, date: new Date()});
     return notes_l;
 };
 
@@ -45,9 +46,12 @@ var edit_note = function () {
 
 };
 
-var display_notes = function (item) {
-    var list_n = list_of_notes.append('<h3>' + item.text + item.date + item.title+'</h3><br>');
-    return list_n
+var display_notes = function () {
+    //var list_n = list_of_notes.append('<p class="bg-info"><h3> Title: ' + item.title +'<br> Text:' + item.text +'<br> Date created:'+ item.date + '</h3></p><hr><br>');
+
+    return $('#list').tmpl(notes_l).appendTo('#table_list');
+
+    //return list_n
 };
 
 var notes_l = get_notes();
@@ -61,9 +65,10 @@ form.submit(function(event){
     add_notes(notes_l);
     set_notes(notes_l);
     $('#list_of_notes').empty();
-    notes_l.forEach(function(item){
-        display_notes(item)
-    });
+
+    //notes_l.forEach(function(item){
+    display_notes(notes_l)
+    //});
 
 });
 
